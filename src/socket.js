5dd24afa0f04244fef4b9db212f723ec1204f1b0
Emre Ekiz <emreekiz90@gmail.com>
2025-07-07 23:25:45 +0300

    socket sunucu adresi güncellendi

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,9 @@
 // client/src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io("https://wordle-server-0xi8.onrender.com", {
   autoConnect: true,
 });
 
 export default socket;
+
