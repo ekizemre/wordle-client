aaef20786eb31f5059db866820588bc0d8c767e4
Emre Ekiz <emreekiz90@gmail.com>
2025-07-08 19:19:12 +0300

    hayvanlar düzenlendi

--- src/kelimeler.js
+++ src/kelimeler.js
@@ -1,5 +1,5 @@
 const kelimeListesi = {
-  hayvanlar: ["aslan", "kapla", "balik", "inek", "karga"],
+  hayvanlar: ["aslan", "serçe", "balik", "inek", "karga"],
   yiyecekler: ["pizza", "pilav", "döner", "makar", "elma"],
   ülkeler: ["türki", "frans", "çinli", "iranl", "japon"],
   teknoloji: ["robot", "lazer", "çipli", "kablo", "ekran"]
